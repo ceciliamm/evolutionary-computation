20d6ef9fbf96bf57d71be57ad85a5f268025ff97
Pablo Trinidad <pabloatp0204@gmail.com>
2019-03-28 01:28:34 -0600

    05 schema creation script added

---
+++ psets/05/pset/create_schema_from_population.py
@@ -0,0 +1,35 @@
+"""Create schema from population."""
+
+import argparse
+import schematax
+import time
+
+
+def compute_schemas(source, output):
+    """Compute schemas."""
+    print('Working with:', source)
+    data = [l.strip('\n') for l in open(source, 'r')]
+    start_time = time.time()
+    schemas = schematax.complete(data)
+    end_time = round(time.time() - start_time, 1)
+
+    print('\tElapsed time:', end_time, 'seconds')
+    print('\t{} schemas obtained'.format(len(schemas)))
+    print('\tWriting result on:', output)
+
+    out = open(output, 'w')
+    out.write('schema,order,defining_length\n')
+    for s in schemas:
+        if str(s) != 'e':
+            line = '{},{},{}\n'.format(str(s), s.get_order(), s.get_defining_length())
+            out.write(line)
+
+    print('Success!')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('population', type=str)
+    parser.add_argument('output', type=str)
+    args = parser.parse_args()
+    compute_schemas(args.population, args.output)
