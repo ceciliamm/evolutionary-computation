d961c07f5dee79bcbabe688ae78c8c3e249af2e1
Pablo Trinidad <pabloatp0204@gmail.com>
2019-04-03 16:08:10 -0600

    P06 | Ex 2 first version added

---
+++ psets/06/pset/algorithms.py
@@ -0,0 +1,70 @@
+"""Algorithms.
+
+This module contain the (1+1)-ES implementation.
+"""
+
+from .individuals import Sphere
+import random
+
+
+class ES1P1:
+    "(1+1) Evolution Strategy algorithm."
+
+    def __init__(
+            self,
+            max_trials: int,
+            precision: float,
+            parent: Sphere,
+            fifth_rule_enabled: bool = False,
+    ) -> None:
+        """Set algorithm parameters."""
+        self.MAX_TRIALS = max_trials
+        self.PRECISION = precision
+        self.WITH_FIFTH_RULE = fifth_rule_enabled
+
+        self.parent = parent
+
+        self.generations = 0
+        self.successful_mutations = []
+        self.fitness_over_time = []
+
+    def run(self) -> None:
+        """Run evolution strategy."""
+        precision = self.parent.fitness
+        while self.generations < self.MAX_TRIALS and precision > self.PRECISION:
+            child = self.mutate(self.parent)
+
+            if child.fitness > self.parent.fitness:
+                precision = child.fitness - self.parent.fitness
+                self.parent = child
+                self.successful_mutations.append(1)
+            else:
+                self.successful_mutations.append(0)
+
+            self.generations += 1
+            self.fitness_over_time.append(self.parent.fitness)
+
+    def mutate(self, parent: Sphere) -> Sphere:
+        """Mutate parent."""
+        child = Sphere(parent.chromosome[:])
+        self.update_std(child)
+        d = len(child.chromosome)
+        for i in range(d):
+            child.chromosome[i] += random.gauss(0, child.std)
+            if child.chromosome[i] < -100:
+                child.chromosome[i] = -100
+            elif child.chromosome[i] > 100:
+                child.chromosome[i] = 100
+        return child
+
+    def update_std(self, individual: Sphere) -> None:
+        """Update individual STD bases on the 1/5 rule."""
+        if self.WITH_FIFTH_RULE:
+            h = len(individual.chromosome)
+            history = self.successful_mutations[-h:]
+            success_rate = sum(history) / len(history) if len(history) > 0 else 0
+
+            if success_rate < 1/5:
+                individual.std *= 0.82
+            elif success_rate > 1/5:
+                individual.std *= 1.22
---
+++ psets/06/pset/individuals.py
@@ -0,0 +1,19 @@
+"""Individuals.
+
+Contain the individuals representation for each problem.
+"""
+
+from typing import List
+
+
+class Sphere:
+    """Individual related to the sphere problem."""
+
+    def __init__(self, chromosome: List[int]) -> None:
+        """Set individual's initial state."""
+        self.chromosome = chromosome
+        self.std = 1.0
+
+    @property
+    def fitness(self):
+        return sum([x**2 for x in self.chromosome])
